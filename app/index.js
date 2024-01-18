da0dc1beef683b4e2d08b175234ba316d39dfb14
Mariya Zaveri <mariyazaveri13@gmail.com>
2024-01-18 18:07:25 +0530

    setting up initial screen

--- app/index.js
+++ app/index.js
@@ -1,12 +1,52 @@
-import { View, Text } from 'react-native';
-
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
 import React from 'react';
 
+import { COLORS, icons, images, SIZES } from '../constants';
+
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from '../components';
+
 function Home(props) {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.lightWhite,
+      }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              dimention='60%'></ScreenHeaderBtn>
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              dimention='100%'></ScreenHeaderBtn>
+          ),
+          headerTitle: '',
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs></Popularjobs>
+          <Nearbyjobs></Nearbyjobs>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
