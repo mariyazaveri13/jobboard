47d0cb5a6179eabed892130b6c6689518aeae423
Mariya Zaveri <mariyazaveri13@gmail.com>
2024-01-18 17:45:36 +0530

    initial commit

---
+++ app/_layout.js
@@ -0,0 +1,11 @@
+import {Stack} from "expo-router"
+
+import React from 'react';
+
+function Layout(props) {
+    return (
+       <Stack></Stack>
+    );
+}
+
+export default Layout;
---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+import React from 'react';
+
+function Home(props) {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+export default Home;
