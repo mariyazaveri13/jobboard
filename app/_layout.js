b9f536e73966f3a8bdc9c6abe6bfc54a274b7a92
Mariya Zaveri <mariyazaveri13@gmail.com>
2024-01-19 12:41:03 +0530

    Welcome component UI finished

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,26 @@
-import {Stack} from "expo-router"
+import { Stack } from 'expo-router';
 
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-function Layout(props) {
-    return (
-       <Stack></Stack>
-    );
-}
+SplashScreen.preventAutoHideAsync();
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView}></Stack>;
+};
 
 export default Layout;
